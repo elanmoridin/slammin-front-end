ba2af9a954947d98154b3a25c9e63d0832bbdcbf
Zach Doll <zach.doll@gmail.com>
2020-08-10 16:00:41 -0500

    added login page and updated methods in auth page

--- slammin-newz/src/helpers/auth.js
+++ slammin-newz/src/helpers/auth.js
@@ -2,8 +2,13 @@
 import { auth } from '../services/firebase'
 // two functions one for signup and one for signin using firebase
 export function signup(email, password) {
-    return auth().createUserWithEmailAndPassword(email, password);
-  }
-  export function signin(email, password) {
-    return auth().signInWithEmailAndPassword(email, password);
-  }
+    return auth().createUserWithEmailAndPassword(email, password)
+}
+export function signin(email, password) {
+    return auth().signInWithEmailAndPassword(email, password)
+}
+
+export function signInWithGoogle() {
+    const provider = new auth.GoogleAuthProvider()
+    return auth().signInWithPopup(provider)
+}
