719b965c5b4ca2668fdb3b450302ab0b8765134d
Zach Doll <zach.doll@gmail.com>
2020-08-12 16:39:16 -0500

    updated world news to be more readable and added my notes

--- slammin-newz/src/components/WorldNews.jsx
+++ slammin-newz/src/components/WorldNews.jsx
@@ -1,21 +1,35 @@
-import React , { Component } from 'react';
-import axios from 'axios';
+import React , { Component } from 'react'
+// import axios for the api call we use
+import axios from 'axios'
 
+// google news api endpoint for everything
 let serverURL = 'https://newsapi.org/v2/everything?' 
+// the call for pro wreslting articles attaches to endpoint call
 let wrestle = 'q=professional+wrestling&'
-let dateMe = 'from=2020-08-12&'
+// grab today's longform date
+let todayDate = new Date()
+// grab today's date in YEAR-MONTH-DAY form for the axios call
+let date = todayDate.getFullYear() + '-' + todayDate.getMonth() + '-' + todayDate.getDate()
+// breaking down the date call into from which comes before the date
+let dateMeFrom = 'from='
+// and call comes after the date
+let dateMeAnd = '&'
+// env api key
 let API_KEY = process.env.REACT_APP_NEWS_API
-console.log(API_KEY)
+// testing api key with env variable
+// console.log(API_KEY)
 
 export default class WorldNews extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
+            // set world news list to null to start for page load
             worldnewslist: null
         }
     }
 
+// old fetch call before switching to axios call
     // findNews = () => {
     // fetch(serverURL + wrestle + dateMe + API_KEY).then(res => {
     //     return res.json();
@@ -25,9 +39,14 @@ export default class WorldNews extends Component {
     // }
 
     componentDidMount(){
-    axios.get(serverURL + wrestle + dateMe + API_KEY)
+    // api call to the google news api with all the variables from up top to create the query string
+    axios.get(serverURL + wrestle + dateMeFrom + date + dateMeAnd + API_KEY)
       .then(res => {
+        // create worldnews arry on mount that can hold the news calls
         const worldnewslist = res.data;
+        // testing the date variable calls
+        // console.log(date)
+        // sets the array to the data.articles to allow mapping over it with react
         this.setState({ worldnewslist: worldnewslist.articles });
       })
     }
@@ -35,12 +54,15 @@ export default class WorldNews extends Component {
     render() {
         return (
         <div>
-            <h2>World News Goes Here</h2>
+            {/* return world news */}
+            <h2>World News</h2>
             {
+                // if world news is null then it lists nothing
                 this.state.worldnewslist == null
                 ?
                 <></>
                 :
+                // else it maps the array and pulls out the info to display it
                 this.state.worldnewslist.map(news => {
                     return (
                         <div className="world-news">
@@ -58,4 +80,4 @@ export default class WorldNews extends Component {
     }
 
 
-// author, content, description, title, url, urlToImage
+// author, content, description, title, url, urlToImage -- what I want to use for the news pulls
